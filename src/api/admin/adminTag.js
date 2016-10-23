482defe5895396552894e6ec8ffb02dd9a8fb61b
cike8899 <18267850322.com>
2016-10-23 23:31:07 +0800

    edit tag api.

--- src/api/admin/adminTag.js
+++ src/api/admin/adminTag.js
@@ -23,11 +23,52 @@ let adminTag = (config, db, Tag, Note) => {
         });
     });
 
-    router.post("/addtag", (req, res, next) => {
-        Tag.create(req.body).then((ret) => {
-            console.info(ret);
-            res.json(ret.dataValues);
+    router.post("/gettagsbypage", (req, res, next) => {
+        let countPerPage = req.body.countPerPage, currentPage = req.body.currentPage;
+        Tag.findAndCountAll({
+            'include': [
+                {
+                    'model': Note
+                }
+            ],
+            'limit': countPerPage,       // 每页多少条
+            'offset': countPerPage * (currentPage - 1)// 跳过多少条
+        }).then((all) => {
+            console.info(all);
+            res.json(all);
         });
+    });
+
+    router.post("/addtag", (req, res, next) => {
+        // Tag.create(req.body).then((ret) => {
+        //     console.info(ret);
+        //     res.json(ret.dataValues);
+        // });
+        let countPerPage = req.body.countPerPage; //currentPage = req.body.currentPage;
+        let preTotal = req.body.preTotal;//之前的条数
+        (async function () {
+            let tag = await Tag.create({ name: req.body.name })
+            if (tag) {
+                let total = preTotal + 1;
+                let remainder = total % 5;
+                let totalPage;
+                if (remainder !== 0) {
+                    totalPage = Math.floor(total / countPerPage) + 1;
+                } else {
+                    totalPage = Math.floor(total / countPerPage);
+                }
+                let lastTags = await Tag.findAndCountAll({
+                    'include': [
+                        {
+                            'model': Note
+                        }
+                    ],
+                    'limit': countPerPage,       // 每页多少条
+                    'offset': countPerPage * (totalPage - 1)// 跳过多少条
+                })
+                res.json({ data: lastTags, operationType: "add" });
+            }
+        })()
     });
 
     router.post("/addbulktags", (req, res, next) => {
@@ -37,7 +78,7 @@ let adminTag = (config, db, Tag, Note) => {
         })()
     });
 
-    router.get("/deltagbyid/:id", (req, res, next) => {
+    router.post("/deltagbyid/:id", (req, res, next) => {
         // co(function* (params) {
         //     let tag = yield Tag.findById(Number(req.params.id));
         //     let ret = yield tag.destroy();
@@ -49,10 +90,40 @@ let adminTag = (config, db, Tag, Note) => {
         // })
 
         async function asyncDelTag() {
-            let tag = await Tag.findById(Number(req.params.id));
-            let ret = await tag.destroy();
+            // let tag = await Tag.findById(Number(req.params.id));
+            let tagId = req.params.id;
+            let current = req.body.current;//当前页码
+            let currentCount = req.body.currentCount;//没删除前，当前页有多少条记录
+            let countPerPage = req.body.countPerPage;//每页有多少条记录
+
+            let delRet = await Tag.destroy({ where: { id: tagId } });//直接通过where条件删除数据不需要先查询后删除。
+
+            let ret;
+            if (delRet === 1) {
+
+                // let currentTotalPage = req.body.totalPage;
+                let skipPage;//需要跳过多少条页面
+                if (currentCount === 1) {
+                    skipPage = current - 2;
+                } else {
+                    skipPage = current - 1;
+                }
+
+                let lastTags = await Tag.findAndCountAll({
+                    'include': [
+                        {
+                            'model': Note
+                        }
+                    ],
+                    'limit': countPerPage,       // 每页多少条
+                    'offset': countPerPage * skipPage// 跳过多少条
+                })
+                ret = lastTags;
+            } else {
+                ret = { success: false };
+            }
             console.info(ret);
-            req.json(ret);
+            res.json(ret);
             console.info(req.params);
         }
         asyncDelTag();
